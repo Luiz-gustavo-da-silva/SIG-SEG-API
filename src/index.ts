f2538c4f78f1efada4400ea1bea6e29e1a89b639
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2025-03-08 23:05:28 -0300

    feat: criação do login e signup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import express, {Application, Request, Response} from 'express'
-import { PORT } from './secrets';
+import express, { Express, Request, Response } from "express";
+import rootRouter from "./router";
+import { PrismaClient } from "@prisma/client";
+import { PORT } from "./secrets";
+import { errorMiddleware } from "./middlewares/errors";
 
-const app: Application = express();
+var cors = require('cors');
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World');
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", rootRouter);
+
+export const prismaCilent = new PrismaClient({
+  log: ["query"],
 });
 
+app.use(errorMiddleware)
+
 app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
