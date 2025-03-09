31f95de8ba86b5d4a6b2d466bb1f97ad8c1b8122
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2025-03-09 00:24:34 -0300

    feat: proteção da rota de update

--- src/router/report.ts
+++ src/router/report.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { createReport, deleteReport, updateReport, findAllReport } from '../controllers/report'
 import { errorHandler } from '../error-handler'
+import authMiddleware from '../middlewares/auth'
 
 const reportRoutes: Router = Router()
 
 reportRoutes.post('/', errorHandler(createReport))
 reportRoutes.delete('/:id', errorHandler(deleteReport))
-reportRoutes.put('/update', errorHandler(updateReport))
+reportRoutes.put('/update', [authMiddleware], errorHandler(updateReport))
 reportRoutes.get('/', errorHandler(findAllReport))
 
 
